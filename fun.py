fe50bca0c9049acd643b7de25b9fde209d03d14f
A. Lose <anlosejhb024@student.wethinkcode.co.za>
2024-11-13 12:58:38 +0200

    Done

--- fun.py
+++ fun.py
@@ -1,3 +1,5 @@
+import math
+
 def dog_years():
     
     """
@@ -10,8 +12,17 @@ def dog_years():
     The dog's age in dog's years is 73
     ```
     """
-
+    calculated_age = 0
+    dog_age = int(input("Input a dog's age in human years: "))
+    for i in range(1, dog_age + 1):
+        if i > 0 and i <=2: 
+            calculated_age += 10.5
+        elif i > 2:
+            calculated_age += 4
+        else:
+            pass
     #enter your code here
+    print(f"The dog's age in dog's years is {int(calculated_age)}")
 
 def fizzbuzz(num):
     """
@@ -23,24 +34,43 @@ def fizzbuzz(num):
     Expected Output:
     fizzbuzz(15) => "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz"
     """
-
     #enter your code here
+    _str = ""
+    for i in range(1, num + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+            _str += "FizzBuzz "
+        elif i % 3 == 0:
+            print("Fizz")
+            _str += "Fizz "
+        elif i % 5 == 0:
+            print("Buzz")
+            _str += "Buzz "
+        else:
+            print(i)
+            _str += f"{i} "    
 
-    
-
+    return _str.strip()
 def word_lengths(sentence):
     """
     Create a program that takes a sentence and returns a dictionary with each unique word
     as the key and the length of the word as the value.
 
-    Expected Output:
+    Expected Output:    for word in sentence.split():
+            _dict[word] = len(word)
     ```
     Input a sentence: "Aunty Yankho is amazing"
     Output: {'Aunty': 5, 'Yankho': 6, 'is': 2, 'amazing': 7}
     ```
     """
-    
     #enter your code here
+    _dict = {}
+    if isinstance(sentence, str): 
+        for word in sentence.split():
+            _dict[word] = len(word)
+    else: 
+        raise ValueError
+    return _dict
 
 def cube_sum(number):
     """
@@ -51,5 +81,11 @@ def cube_sum(number):
     cube_sum(123) => 1^3 + 2^3 + 3^3 = 1 + 8 + 27 = 36
     ```
     """
-    
     #enter your code here
+    _sum = 0
+    str_num = str(number)
+    for num in str_num:
+        _sum += math.pow(int(num), 3)
+    return int(_sum)
+
+cube_sum(-123)
